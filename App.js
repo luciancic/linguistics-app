f92b9bcb202298e6f9338dbe40a31d811ede95be
Lucian Condrea <luciancondreadev@gmail.com>
2019-10-13 18:32:17 -0400

    spacing

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
 import ListenScreen from './src/components/ListenScreen';
 import HomeScreen from './src/components/HomeScreen';
 import SpeakScreen from './src/components/SpeakScreen';
 import CollectionScreen from './src/components/CollectionScreen';
 
 const AppNavigator = createStackNavigator({
-    Home: HomeScreen,
-    Listen: ListenScreen,
-    Speak: SpeakScreen,
-    Collection: CollectionScreen,
+  Home: HomeScreen,
+  Listen: ListenScreen,
+  Speak: SpeakScreen,
+  Collection: CollectionScreen,
 });
 
 export default createAppContainer(AppNavigator);
